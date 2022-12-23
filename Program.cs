e098af09b9ba94e4b806cac89173ae992430c627
Karen Harutyunyan <karenvardanovich@gmail.com>
2022-12-23 18:18:52 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+class A
+{
+    public string name = "CAR";
+    protected string name2 = "BMW";
+}
+class B : A
+{
+    public  void Print()
+    { 
+        Console.WriteLine(name);
+        Console.WriteLine(name2);
+    }
+}
+class C : B
+{
+  
+}
+class Program
+{
+    static void Main()
+    {
+        
+        B b = new B();
+
+        b.name = "Muk";
+        // b.name2 stex chi erevum, qani vor Program classy jarangvac chi A class ic 
+
+        b.Print();
+
+        Console.ReadKey();
+
+    }
+
+}
+
+
